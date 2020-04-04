64f21ee0304193b1a432ac850fe42e2b0832f5d7
Yuki Chen <yingchen@chapman.edu>
2020-04-03 18:10:40 -0700

    add main.cpp

---
+++ GenStackMain.cpp
@@ -0,0 +1,118 @@
+
+// https://www.geeksforgeeks.org/print-stack-elements-from-bottom-to-top/
+//https://www.techiedelight.com/stack-implementation-using-templates/
+
+
+#include "GenStack.cpp"
+#include<fstream>
+
+
+using namespace std;
+
+int main() {
+   bool fileOk=true;
+   string inputfileName;
+   cout << "enter a file name" << endl;
+   cin >> inputfileName;
+
+  while(fileOk){
+  //initialize variables
+  string Line;
+  double totalLine=0;
+  string a;
+  GenStack *Stack1 = new GenStack(10);
+  GenStack *Stack2 = new GenStack(10);
+  GenStack *Stack3 = new GenStack(10);
+
+  ifstream InputFile;
+  InputFile.open(inputfileName);
+  //if input file doesn't exist
+  if(!InputFile)
+  {
+    //output error message
+    cerr << "unable to open file" << endl;
+    exit(1);
+  } // end if
+
+  //input file successfully opened
+  else if(InputFile)
+  {
+    while (getline(InputFile, Line))
+    {
+      //keep track of the line the error is at
+      totalLine++;
+      //loop through each line and add {} [] () in array
+      for(int z=0; z < Line.length(); z++)
+      {
+        //divide each line into single letters
+           a = Line.substr(z,1);
+          cout << Line[z] << endl;
+          //put ( [ { in different stacks to pop
+          if(a=="("){
+            Stack1->push('(');
+          }//end if
+          else if(a=="["){
+            Stack2->push('[');
+          }//end else if
+          else if(a=="{"){
+            Stack3->push('{');
+          }//end else if
+          //check whether Stack1 is empty before popping
+          else if (a==")"){
+            if (!Stack1->isEmpty()){
+              Stack1->pop();
+          }// end check empty if
+           else{
+             cout << "found an extra ')' in line " << totalLine << endl;
+             exit(1);
+           }// end else
+        }//end else if
+
+        else if (a=="]"){
+          if (!Stack2->isEmpty()){
+            Stack2->pop();
+        }// end check empty if
+         else{
+           cout << "found an extra ']' in line " << totalLine << endl;
+           exit(1);
+         }// end else
+      }//end else if
+
+      else if (a=="}"){
+        if (!Stack3->isEmpty()){
+          Stack3->pop();
+      }// end check empty if
+       else{
+         cout << "found an extra '}' in line " << totalLine << endl;
+         exit(1);
+       }// end else
+    }//end else if
+      }//end for
+    } // end while
+    if (Stack1->isEmpty()){
+      cout << "reach end of file, missing ')' in line " << totalLine << endl;
+    }
+    cout << "totalLine: " << totalLine << endl;
+
+  }// end else if
+
+  string answer;
+  cout << "do you want to open another file and process? enter 'yes' or 'no' " << endl;
+  cin >> answer;
+
+  if (answer=="yes")
+  {
+    cout << "enter your file name " << endl;
+    cin >> inputfileName;
+  }// end if
+  else if (answer=="no")
+  {
+    //exit the program when user chooses no
+    cout << "exit the program " << endl;
+    fileOk = false;
+  } // end else if
+}// end fileOK while
+
+  return 0;
+
+}// end main
